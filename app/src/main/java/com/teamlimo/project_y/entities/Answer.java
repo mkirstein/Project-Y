1f9bd574ac82ce8efbafe9e6460a03de83337cc8
Project 0rion <ezio.ac2@arcor.de>
2016-03-28 18:46:32 +0200

    Answer & Question Attribute hinzugefügt

--- app/src/main/java/com/teamlimo/project_y/entities/Answer.java
+++ app/src/main/java/com/teamlimo/project_y/entities/Answer.java
@@ -4,4 +4,7 @@ package com.teamlimo.project_y.entities;
  * Created by Project0rion on 28.03.2016.
  */
 public class Answer {
+
+    private String text;
+    private boolean isCorrect;
 }
--- app/src/main/java/com/teamlimo/project_y/entities/Question.java
+++ app/src/main/java/com/teamlimo/project_y/entities/Question.java
@@ -1,7 +1,12 @@
 package com.teamlimo.project_y.entities;
 
+import java.util.ArrayList;
+
 /**
  * Created by Project0rion on 28.03.2016.
  */
 public class Question {
+
+    private String text;
+    private ArrayList<Answer> answers;
 }

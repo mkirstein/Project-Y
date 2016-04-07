729cba898637977ac43db57a68183710c0576d19
pex1l <marc.kirstein@gmail.com>
2016-04-07 16:09:06 +0200

    Backbutton Verhalten überschrieben => Zurück zum Hauptmenü

--- app/src/main/java/com/teamlimo/project_y/quizresult/QuizResultActivity.java
+++ app/src/main/java/com/teamlimo/project_y/quizresult/QuizResultActivity.java
@@ -2,9 +2,12 @@ package com.teamlimo.project_y.quizresult;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.teamlimo.project_y.R;
+import com.teamlimo.project_y.core.IViewManager;
 import com.teamlimo.project_y.core.PresenterFactory;
+import com.teamlimo.project_y.core.ViewManager;
 
 /**
  * Created by Marc on 07.04.2016.
@@ -24,5 +27,16 @@ public class QuizResultActivity extends Activity implements IQuizResultView {
     protected void onStart() {
         super.onStart();
         presenter.setView(this);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+            IViewManager vM = ViewManager.getInstance();
+            vM.switchView(this, vM.getViewFactory().createMenuView());
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
